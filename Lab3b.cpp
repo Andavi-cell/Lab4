74acadda00888912dc75543f640b396673560dce
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2023-01-19 11:36:50 -0500

    Initial

---
+++ Lab3b.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double yourHeight, distance, angle, buildingHeight;
+    // your solution code goes here
+   
+    cout << "The height of the building is " << buildingHeight << " feet tall.\n";
+    return 0;
+}
