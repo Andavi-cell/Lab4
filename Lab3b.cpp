35edee5c40174b4c8d5d630f1bfeb5719539f7bd
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2023-09-17 17:14:45 -0400

    Initial Commit

--- Lab3b.cpp
+++ Lab3b.cpp
@@ -7,6 +7,5 @@ int main()
     double yourHeight, distance, angle, buildingHeight;
     // your solution code goes here
    
-    cout << "The height of the building is " << buildingHeight << " feet tall.\n";
     return 0;
 }
