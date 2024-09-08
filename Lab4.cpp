bf441eb7de0b450a5322e60cc6fd45bc8160bc7e
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2024-09-08 16:58:47 -0400

    Initial Commit

--- Lab4.cpp
+++ Lab4.cpp
@@ -2,6 +2,15 @@
 #include <cmath>
 using namespace std;
 
+/*
+Complete the given template by creating the code that calculates 
+the height of a building to the nearest integer when given the height 
+of the person, distance from the building, and the angle from the 
+line of sight to the top of the building. Note: the C++ tan function 
+accepts the angle in radians, so your program will have to convert 
+degrees to radians using the factor: radians = degrees * (π/180)
+*/
+
 int main()
 {
     double yourHeight, distance, angle, buildingHeight;
