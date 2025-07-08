dace267219a14b16b2c18748e0aa9f94754e1f6e
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2025-07-08 11:39:52 -0400

    Initial Commit

--- Lab4.cpp
+++ Lab4.cpp
@@ -1,20 +1,11 @@
 #include <iostream>
-#include <cmath>
+#include <cmath> // library providing trig functions as well as M_PI global constant
 using namespace std;
-
-/*
-Complete the given template by creating the code that calculates 
-the height of a building to the nearest integer when given the height 
-of the person, distance from the building, and the angle from the 
-line of sight to the top of the building. Note: the C++ tan function 
-accepts the angle in radians, so your program will have to convert 
-degrees to radians using the factor: radians = degrees * (π/180)
-*/
 
 int main()
 {
     double yourHeight, distance, angle, buildingHeight;
-    // your solution code goes here
+    // Place your solution code here for Lab4 and please use the given variables above 
    
     return 0;
 }
